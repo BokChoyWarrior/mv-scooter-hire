79b9036872bac7589110fad2f9cff5877985f67f
Harvey Cambridge <harvey.cambridge@hindsightsoftware.co.uk>
2021-10-18 16:29:09 +0100

    Add TODO tests

--- tests/User.test.ts
+++ tests/User.test.ts
@@ -12,14 +12,22 @@ describe('the User (and Person) class', () => {
 
   afterEach(teardownMockEnvironment);
 
-  test('the user can register', () => {
+  it('the user can register', () => {
     let person = new Person('harv', 55, new Location());
     let user = person.register();
     expect(user).toBeInstanceOf(User);
   });
 
-  test('"underage users cannot register', () => {
+  it('underage users cannot register', () => {
     let underage = new Person('person 2', 5, new Location());
     expect(() => underage.register()).toThrowError();
   });
+
+  it.todo('The user can add payment details');
+
+  it.todo('the user can hire a scooter');
+
+  it.todo("the user can return the scooter to a station, and money will be taken form user's account");
+
+  it.todo('the user can mark a scooter as broken, and can contact team to get a refund');
 });
