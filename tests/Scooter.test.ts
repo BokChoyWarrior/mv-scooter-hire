dc804d55394fe4bb5f37a5b38859f44b41a770ac
Harvey Cambridge <49193507+BokChoyWarrior@users.noreply.github.com>
2021-10-20 14:31:15 +0100

    Cleanup of scooter tests

--- tests/Scooter.test.ts
+++ tests/Scooter.test.ts
@@ -29,7 +29,6 @@ describe('the Scooter class', () => {
     const promiseB = scooter.charge();
     expect(promiseA).toBe(promiseB); // We use toBe() to check reference, not value
 
-    // WORKS
     const promise = scooter
       .charge()
       .then(() => {
@@ -38,16 +37,12 @@ describe('the Scooter class', () => {
       })
       .catch();
 
-    jest.runAllTimers();
+    jest.advanceTimersByTime(10000);
     return promise;
-    // Also works :)
-    // await scooter.charge();
-    // expect(scooter.batteryPercent).toBe(100);
-    // expect(scooter.isAvailable).toBe(true);
   });
 
   // This is essentially the same test as above, but for fixing rather than charging
-  it('should be fix properly', () => {
+  it('should be fixed properly', () => {
     const scooter = new Scooter();
     expect(scooter.isBroken).toBe(false);
 
@@ -73,74 +68,85 @@ describe('the Scooter class', () => {
   });
 
   it('should set itself available when charged and fixed', async () => {
-    let scooterAlerter = new Scooter();
-    scooterAlerter.isBroken = true;
-    scooterAlerter.batteryPercent = 0;
+    const station = new DockingStation(new Location());
+    const scooter = new Scooter();
+    scooter.batteryPercent = 0;
+    scooter.isBroken = true;
+
+    // Dock the broken scooter
+    station.dock(scooter, true);
 
     // Assign an event listener for the scooter to tell us when it's available
-    scooterAlerter.once('available', () => {
-      expect(!!scooterAlerter.batteryPercent && !scooterAlerter.isBroken).toBe(true);
-      expect(scooterAlerter.isAvailable).toBe(true);
+    let availableFired = false;
+    scooter.once('available', () => {
+      expect(!!scooter.batteryPercent && !scooter.isBroken).toBe(true);
+      expect(scooter.isAvailable).toBe(true);
+      availableFired = true;
     });
 
     // Make sure scooter unavailable
-    expect(scooterAlerter.isAvailable).toBe(false);
-    let p1 = scooterAlerter.fix();
-    let p2 = scooterAlerter.charge();
+    let p1 = scooter.fix();
+    let p2 = scooter.charge();
     // Once both these promises resolve, the event should have already fired and called callback above, so test should pass.
     const promise = Promise.all([p1, p2]);
     jest.advanceTimersByTime(5000);
+
+    // making sure that the event fired
+    expect(availableFired).toBe(true);
     return promise;
   });
 
-  it('should discharge while hired, and charge when docked', async () => {
+  it('should charge while docked', () => {
     const station = new DockingStation(new Location());
-    const s1 = new Scooter();
+    const scooter = new Scooter();
     const user = new User('1', 100, new Location());
-    station.dock(s1);
 
-    const nearest = user.findNearestAvailableStation();
-    // console.log(nearest);
-    expect(nearest).toBe(station);
+    scooter.batteryPercent = 1;
 
-    let scooter: Scooter;
+    jest.advanceTimersByTime(1000);
+    expect(scooter.batteryPercent).toBe(1);
 
-    if (!nearest) {
-      return;
-    }
-
-    // Testing discharging
-    scooter = user.hireFrom(nearest);
-
-    expect(scooter.batteryPercent).toBe(100);
-
-    //wait 0.2 secs (or 0.2 hours)
-    jest.advanceTimersByTime(2000);
-    expect(scooter.batteryPercent).toBeLessThan(100);
-    nearest.dock(user);
-
-    // wait for charge
-    jest.advanceTimersByTime(2000);
+    station.dock(scooter);
+    jest.advanceTimersByTime(10000);
     expect(scooter.batteryPercent).toBe(100);
   });
 
-  test('the scooter should not pass 0 battery level', async () => {
+  it('should discharge while hired', async () => {
     const station = new DockingStation(new Location());
-    const s1 = new Scooter();
-    station.dock(s1);
+    const scooter = new Scooter();
+    const user = new User('1', 100, new Location());
+    station.dock(scooter);
+    const hiredScooter = user.hireFrom(station);
 
+    expect(hiredScooter.batteryPercent).toBe(100);
+
+    jest.advanceTimersByTime(10000);
+    expect(hiredScooter.batteryPercent).toBeLessThan(100);
+
+    user.dock(station, false);
+
+    jest.advanceTimersByTime(10000);
+    expect(hiredScooter.batteryPercent).toBe(100);
+  });
+
+  test('should not pass 0 battery level', async () => {
+    const station = new DockingStation(new Location());
+    const scooter = new Scooter();
     const user = new User('test', 48930242, new Location());
 
+    station.dock(scooter);
     user.hireFrom(station);
+
     jest.advanceTimersByTime(5000);
 
-    expect(s1.batteryPercent).toBe(0);
+    expect(scooter.batteryPercent).toBe(0);
   });
 
-  test('the scooter should not be hired after it is docked', () => {
+  it('should not be hired after it is docked', () => {
     const user = new User('1', 50, new Location());
     const station = new DockingStation(new Location());
     const scooter = new Scooter();
+
     station.dock(scooter);
 
     expect(scooter.isHired).toBe(false);
